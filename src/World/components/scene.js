745f0feacaf4bbb1a5813e85ff47871eb5c8a4b8
polar0 <23r0eth@gmail.com>
2022-05-02 22:17:28 +0200

    Update scene background

--- src/World/components/scene.js
+++ src/World/components/scene.js
@@ -2,7 +2,7 @@ import { Scene, Color, Fog } from 'three';
 
 function createScene() {
   const scene = new Scene();
-  scene.background = new Color('#353434');
+  scene.background = new Color('#000000');
   scene.fog = new Fog(0x000000, 1, 30);
 
   return scene;
