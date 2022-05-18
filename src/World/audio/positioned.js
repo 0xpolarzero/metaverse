0f79560a82bfd3719adf604251212c1b00f3d7d4
polar0 <23r0eth@gmail.com>
2022-05-18 18:19:10 +0200

    Fixed a bug that would desync audio files

--- src/World/audio/positioned.js
+++ src/World/audio/positioned.js
@@ -6,14 +6,16 @@ async function loadSFX(objArray) {
   let sfxBoule1 = {};
   sfxBoule1.url = './assets/audio/positioned_pad.wav';
   let sfxBoule2 = {};
-  sfxBoule2.url = './assets/audio/positioned_key.wav';
+  sfxBoule2.url = './assets/audio/positioned_perc.wav';
   let sfxBoule3 = {};
-  sfxBoule3.url = './assets/audio/positioned_perc.wav';
+  sfxBoule3.url = './assets/audio/positioned_key.wav';
   const sfxArray = [sfxBoule1, sfxBoule2, sfxBoule3];
 
   // Create an object for each sound with its own properties
   for (let i = 0; i < objArray.length; i++) {
     sfxArray[i] = createSource(sfxArray[i].url, objArray[i]);
+  }
+  for (let i = 0; i < objArray.length; i++) {
     sfxArray[i].playSource();
   }
 
