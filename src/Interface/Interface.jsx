26e502d616b99188f32ee69c6a74f18613c96a87
polarzero <0xpolarzero@gmail.com>
2022-12-14 11:26:58 +0100

    Complete interface & add Leva

--- src/Interface/Interface.jsx
+++ src/Interface/Interface.jsx
@@ -1,16 +1,33 @@
 import Crosshair from './components/Crosshair';
 import useInterface from '../stores/Interface';
 import { useState } from 'react';
+import { Leva, useControls } from 'leva';
 
 const Interface = () => {
   const [hoverEnter, setHoverEnter] = useState(false);
   const { showMenu } = useInterface();
+
+  const { speed } = useControls({
+    speed: {
+      value: 1,
+      min: 0,
+      max: 10,
+      step: 0.1,
+    },
+  });
+  // Randomize positions
+  // Reverb amount (min 0 max 100 value 20)
+  // Externalizer / Room character (min 0 max 100 value 50) - warm neutral airy
+  // Externalizer intensity (min 0 max 100 value 30)
 
   return (
     <>
       <Crosshair />
       {/* Show the menu on pointer unlocked */}
       <div id='overlay' className={showMenu ? 'visible' : ''}>
+        <div className='leva-wrapper' onClick={(e) => e.stopPropagation()}>
+          <Leva />
+        </div>
         <div
           className='interface'
           //   Let the user know that any click not on another button will enter the game
@@ -20,11 +37,47 @@ const Interface = () => {
               : setHoverEnter(true)
           }
         >
-          <div className='action'>
+          <div className='wrapper'>
             <div className={hoverEnter ? 'mock-btn active' : 'mock-btn'}>
               Click to enter
             </div>
-            <button>ttt</button>
+            <div className='interface__content'>
+              <div className='interface__instructions'>
+                <span>Move</span>
+                <span className='interface__keys'>
+                  <span className='key'>W</span>
+                  <span className='key'>A</span>
+                  <span className='key'>S</span>
+                  <span className='key'>D</span>{' '}
+                  <span className='separator'>/</span>{' '}
+                  <span className='key'>↑</span>
+                  <span className='key'>←</span>
+                  <span className='key'>↓</span>
+                  <span className='key'>→</span>
+                </span>
+                <span>Interact</span>
+                <span className='interface__keys'>
+                  <span className='key'>Click</span>{' '}
+                  <span className='separator'>/</span>{' '}
+                  <span className='key'>E</span>
+                </span>
+                <span>Menu</span>
+                <span className='interface__keys'>
+                  <span className='key'>ESC</span>
+                </span>
+              </div>
+            </div>
+            <div className='interface__credits'>
+              Audio powered by{' '}
+              <a
+                href='https://atmoky.com/'
+                target='_blank'
+                rel='noopener noreferrer'
+                onClick={(e) => e.stopPropagation()}
+              >
+                Atmoky
+              </a>
+            </div>
           </div>
         </div>
       </div>
